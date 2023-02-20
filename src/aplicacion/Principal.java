07ec658580a43d716cb0588e792e7880a11cd25b
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:18:41 +0100

    ej5 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_5;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        double [] lista= {1,2,3};
+        int i;
+        System.out.println("La media de la lista es "+ej2_5.media(lista,i=lista.length-1)/lista.length);
+    }
 }
--- src/dominio/ej2_5.java
+++ src/dominio/ej2_5.java
@@ -1,2 +1,16 @@
-package dominio;public class ej2_5 {
+package dominio;
+
+public class ej2_5 {
+
+
+    public static double media(double[] lista,int i) {
+        if(i==0) {
+            return lista[i];
+        }else {
+
+            return 	lista[i]+media(lista,i-1);
+        }
+
+    }
+
 }
